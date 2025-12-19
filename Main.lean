2d2fd6d08e0efb90b41eb673d9b07b8e560d3d32
Nick_Adfor <NickAdfor@outlook.com>
2025-12-19 23:16:16 +0800

    Add files via upload

---
+++ Main.lean
@@ -0,0 +1,4 @@
+import ThePolynomialMethod
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ lakefile.lean
@@ -0,0 +1,7 @@
+import Lake open Lake DSL
+
+require mathlib from git "https://github.com/leanprover-community/mathlib4.git" @ "v4.26.0-rc2"
+
+package "polynomial_method" where -- package configuration options here
+
+lean_lib "PolynomialMethod" where -- library configuration options here
