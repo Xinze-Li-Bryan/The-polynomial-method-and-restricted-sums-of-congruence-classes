02efbe1310b921fff7902f6ecc8b8f2d10642664
Nick Adfor <NickAdfor@outlook.com>
2026-01-18 22:54:52 +0800

    Setup blueprint

--- lakefile.lean
+++ lakefile.lean
@@ -7,3 +7,9 @@ package "ThePolynomialMethod" where
 lean_lib "ThePolynomialMethod" where
   srcDir := "."
   globs := #[.submodules `ThePolynomialMethod]
+
+require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
+
+meta if get_config? env = some "dev" then
+require «doc-gen4» from git
+  "https://github.com/leanprover/doc-gen4" @ "main"
